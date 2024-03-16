945fed116b9e690e1bf4a93fed83bfc238dc3bf0
nafis.exe <nafis.ali0123@gmail.com>
2024-03-16 18:29:02 +1100

    Add socket emission for new user in Home component
    The Home component has been updated to emit a "newUser" event via the provided socket whenever a new user is created. This includes sending the username and socket ID. This change allows real-time tracking of users as they join the chat application, improving interactivity and user management.

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-const Home = () => {
+const Home = ({ socket }) => {
   const navigate = useNavigate();
   const [ userName, setUserName ] = useState("");
 
@@ -9,6 +9,10 @@ const Home = () => {
     event.preventDefault();
 
     localStorage.setItem("userName", userName);
+    socket.emit("newUser", {
+      userName,
+      socketID: socket.id,
+    });
 
     navigate("/chat");
   };
