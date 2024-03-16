cbb495e3b074e099b06b4e4c99f71e8f8a7b7c84
nafis.exe <nafis.ali0123@gmail.com>
2024-03-16 13:03:32 +1100

    Integrate React Router in App component
    The commit introduces the use of React Router in the App component. The BrowserRouter, Routes, and Route components from 'react-router-dom' have been imported. A new Home component has also been added to the project and is now being rendered as a route at the root path ("/"). This change allows for better navigation management within our application.

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,20 @@
 import "./App.css";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
 import socketIO from "socket.io-client";
 
 const socket = socketIO.connect("http://localhost:4000");
 
 function App() {
   return (
-    <div>
-      <p>Hello World!</p>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home socket={socket} />}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
