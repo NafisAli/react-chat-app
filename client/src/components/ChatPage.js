d984d0abc44b167a67641778d7d46c523a21a797
nafis.exe <nafis.ali0123@gmail.com>
2024-03-16 18:33:28 +1100

    Add auto-scroll to latest chat message
    Added a feature that automatically scrolls the chat window to the most recent message. This was achieved by introducing a useRef hook in ChatPage.js, which is then passed as a prop to ChatBody.js. The ref is attached to an empty div at the end of the messages list and scrolled into view whenever new messages are added. This improves user experience by ensuring users always see the latest messages without manually scrolling down.

--- client/src/components/ChatPage.js
+++ client/src/components/ChatPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 
 import ChatBar from "./ChatBar";
 import ChatBody from "./ChatBody";
@@ -6,16 +6,21 @@ import ChatFooter from "./ChatFooter";
 
 const ChatPage = ({ socket }) => {
   const [ messages, setMessages ] = useState([]);
+  const lastMessageRef = useRef(null);
 
   useEffect(()=> {
     socket.on("messageResponse", data => setMessages([...messages, data]));
   }, [socket, messages]);
 
+  useEffect(() => {
+    lastMessageRef.current?.scrollIntoView({ behaviour: "smooth"});
+  }, [messages]);
+
   return (
     <div className="chat">
       <ChatBar socket={socket} />
       <div className="chat-main">
-        <ChatBody messages={messages} />
+        <ChatBody messages={messages} lastMessageRef={lastMessageRef} />
         <ChatFooter socket={socket} />
       </div>
     </div>
