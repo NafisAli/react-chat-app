60bbd53fadc9281c2b6e1856234f95866a97a8ec
nafis.exe <nafis.ali0123@gmail.com>
2024-03-16 13:14:28 +1100

    Add ChatBar component
    Introduced a new ChatBar component to the client side. This component will serve as the sidebar for open chats, displaying active users. The purpose of this addition is to enhance user interaction and engagement within the application.

---
+++ client/src/components/ChatBar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const ChatBar = () => {
+  return (
+    <div  className="chat-sidebar">
+      <h2>Open Chat</h2>
+      <div>
+        <h4 className="chat-header">Active Users</h4>
+        <div className="chat-users">
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChatBar;
