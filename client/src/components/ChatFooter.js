b8dc2cfeb25103c47363f371e93ed1f5589fd7c9
nafis.exe <nafis.ali0123@gmail.com>
2024-03-16 13:32:15 +1100

    Add ChatFooter component for message input
    This commit introduces a new ChatFooter component. This component includes an input field and a send button, allowing users to write and submit messages. The state of the message is managed using React's useState hook. Upon form submission, the user's name (retrieved from localStorage) and their message are logged to the console, and the message state is reset.

---
+++ client/src/components/ChatFooter.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const ChatFooter = () => {
+  const { message, setMessage } = useState("");
+
+  const handleSetMessage = (event) => {
+    event.preventDefault();
+
+    console.log({ userName: localStorage.getItem("userName"), message });
+    setMessage("");
+  };
+
+  return (
+    <div className="chat-footer">
+      <form className="form" onSubmit={handleSetMessage}>
+        <input
+          type="text"
+          placeholder="Write message"
+          className="message"
+          value={message}
+          onChange={(event) => setMessage(event.target.value)}
+        />
+        <button className="send-btn">Send</button>
+      </form>
+    </div>
+  )
+}
